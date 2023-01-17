ed41f317d324c7acffc4b056c64bfc6c8fa465e3
FELLAH-tech <hicham8fellah@gmail.com>
2023-01-17 18:12:35 +0100

    working auth+back

---
+++ install.sh
@@ -0,0 +1,10 @@
+npm install; 
+npm install express; 
+npm install pug; 
+npm install express-session; 
+npm install connect-sqlite3; 
+npm install sqlite3 sqlite; 
+node install.js; 
+node db.js; 
+node db_debug.js; 
+npm start; 
