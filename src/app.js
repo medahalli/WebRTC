b508c9b6348a697eee8090b3b9cff7597223eafd
AHALLI Mohamed <Mohamed.Ahalli@bordeaux-inp.fr>
2023-01-19 04:50:52 +0100

    password crypted

--- src/app.js
+++ src/app.js
@@ -2,6 +2,7 @@ let express = require( 'express' );
 let app = express();
 let stream = require( './ws/stream' );
 let path = require( 'path' );
+const bcrypt = require('bcrypt');
 
 var sqlite3 = require('sqlite3').verbose();
 var db = new sqlite3.Database('Data_Base.db');
@@ -88,7 +89,7 @@ app.get('/inscription', (req, res)=>{
 
 });
 
-app.post('/inscription', (req, res)=>{
+/*app.post('/inscription', (req, res)=>{
   // Checking info & saving data
   db.run(`
       INSERT INTO Users(pseudo, email, password)
@@ -96,7 +97,7 @@ app.post('/inscription', (req, res)=>{
           (?, ?, ?);
   `, req.body.pseudo, req.body.email, req.body.password);
   res.redirect('/');
-});
+});*/
 
 app.get('/authen', (req, res)=>{
 
@@ -107,19 +108,113 @@ app.get('/authen', (req, res)=>{
 
 });
 
-app.post('/authen', (req, res)=>{
+
+
+
+
+app.post('/inscription', (req, res)=>{
+  // Génération du sel
+  const saltRounds = 10;
+  bcrypt.genSalt(saltRounds, function(err, salt) {
+    // Chiffrement du mot de passe
+    bcrypt.hash(req.body.password, salt, function(err, hash) {
+      // Sauvegarde des informations dans la base de données
+      db.run(`
+          INSERT INTO Users(pseudo, email, password)
+          VALUES
+              (?, ?, ?);
+      `, req.body.pseudo, req.body.email, hash);
+      res.redirect('/');
+    });
+  });
+});
+
+
+
+/*app.post('/authen', (req, res)=>{
+    // Récupération du mot de passe stocké dans la base de données
+    db.get(`
+        SELECT * FROM Users WHERE email = ?;
+    `, req.body.email, (err, row) => {
+      if (row) {
+        // Comparaison du mot de passe entré avec celui stocké chiffré
+        bcrypt.compare(req.body.password, row.password, function(err, match) {
+          if (match) {
+            // Si les mots de passe correspondent, connecter l'utilisateur
+            req.session.authenticated = true;
+            res.redirect('/');
+          } else {
+            // Sinon, afficher un message d'erreur
+            res.render('authen.pug', {err_msg: "Mot de passe incorrect"});
+          }
+        });
+      } else {
+        // Si l'utilisateur n'est pas trouvé, afficher un message d'erreur
+        res.render('authen.pug', {err_msg: "Utilisateur non trouvé"});
+      }
+    });
+  });*/
+
+
+
+
+  app.post('/authen', (req, res)=>{
+    // Récupération des informations de l'utilisateur
+    err_msg = "2892";
+    err = false;
+    db.get("SELECT * FROM Users WHERE email = ?", [req.body.email],
+    (err, row)=>{
+        if(typeof row === 'undefined'){
+            err_msg = "There is no user with this email";
+            err = true;
+        }
+    
+        // Comparaison des mots de passe
+        bcrypt.compare(req.body.password, row.password, function(err, result) {
+        if(result == true) {
+        // Connexion de l'utilisateur
+            req.session.authenticated = true;
+            req.session.email = row.email
+            req.session.user_id = row.id
+            req.session.pseudo = row.pseudo
+            res.redirect('/');
+        } else {
+        // Erreur de mot de passe
+                data = {
+                    err_msg : "Mot de passe incorrect"
+                }
+                res.render('authen.pug', data);
+        }
+        });
+         
+    });
+    });
+
+
+
+
+
+  
+
+/*app.post('/authen', (req, res)=>{
   // Checking info & saving data
   err_msg = "2892";
   err = false;
-  db.get("SELECT * FROM Users WHERE email = ?", [req.body.email],
+  db.get("SELECT password FROM Users WHERE email = ?", [req.body.email],
   (err, row)=>{
       if(typeof row === 'undefined'){
           err_msg = "There is no user with this email";
           err = true;
-      }else if(row.password != req.body.password){
-          err_msg = "Wrong password";
-          err = true;
       }
+      console.log(req.body.password);
+      console.log(row.password);
+      bcrypt.compare(req.body.password, row.password, function(err, result) {
+        if(result === false) {
+            err_msg = "Wrong password";
+            err = true;
+        }
+      })
+      
 
       if(err){
           data = {
@@ -133,7 +228,7 @@ app.post('/authen', (req, res)=>{
           res.redirect('/');
       }
   });
-});
+});*/
 
 
 app.get('/deconnect', (req, res)=>{
